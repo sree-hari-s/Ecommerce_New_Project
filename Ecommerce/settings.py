5cd37608fd6f274faea93987e6beea1faea8f0ae
Sreehari <sreeharis1999@gmail.com>
2023-01-03 17:26:20 +0530

    allowed hosts

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default = True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
