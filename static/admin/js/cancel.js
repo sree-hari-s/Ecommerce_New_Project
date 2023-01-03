45a821fa98a702d5f2cd7600ac186bfdfbcea54e
Sreehari <sreeharis1999@gmail.com>
2023-01-03 17:42:36 +0530

    static files

--- static/admin/js/cancel.js
+++ static/admin/js/cancel.js
@@ -1,29 +1,17 @@
-'use strict';
-{
-    // Call function fn when the DOM is loaded and ready. If it is already
-    // loaded, call the function now.
-    // http://youmightnotneedjquery.com/#ready
-    function ready(fn) {
-        if (document.readyState !== 'loading') {
-            fn();
-        } else {
-            document.addEventListener('DOMContentLoaded', fn);
-        }
-    }
+(function($) {
+    'use strict';
 
-    ready(function() {
-        function handleClick(event) {
-            event.preventDefault();
-            const params = new URLSearchParams(window.location.search);
-            if (params.has('_popup')) {
-                window.close(); // Close the popup.
+    $(document).ready(function() {
+        $('.cancel-link').click(function(e) {
+            e.preventDefault();
+            const parentWindow = window.parent;
+            if (parentWindow && typeof(parentWindow.dismissRelatedObjectModal) === 'function' && parentWindow !== window) {
+                parentWindow.dismissRelatedObjectModal();
             } else {
-                window.history.back(); // Otherwise, go back.
+                // fallback to default behavior
+                window.history.back();
             }
-        }
-
-        document.querySelectorAll('.cancel-link').forEach(function(el) {
-            el.addEventListener('click', handleClick);
+            return false;
         });
     });
-}
+})(django.jQuery);
