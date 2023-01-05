976dd6d6d6e6ea6049310f3acb4ac8130a445497
Sreehari <sreeharis1999@gmail.com>
2023-01-05 23:47:18 +0530

    images

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -128,10 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-    'Ecommerce/static',
-]
 
+STATICFILES_DIRS = ['Ecommerce/static',]
+    
 # media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -18,9 +18,13 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
+    url(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
+    
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
